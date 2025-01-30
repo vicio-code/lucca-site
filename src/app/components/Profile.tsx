57cc9ca629a0046c39a3f5f01784ee2e4055520f
vicio-code <luccafalbo@gmail.com>
2025-01-30 14:52:19 -0300

    feat: :sparkles: Implement profile component

---
+++ src/app/components/Profile.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import profilePic from "@/assets/images/batman.jpeg";
+import Link from "next/link";
+import BriefcaseIcon from "@/assets/icons/BriefcaseIcon";
+import LocationIcon from "@/assets/icons/LocationIcon";
+
+const data = {
+  Name: "lucca falbo",
+  Job: "Procurando oportunidades",
+  Location: "Belo Horizonte, BR",
+  JobTitle: "Desenvolvedor web",
+  Description: [
+    "I like building and breaking stuff to see how it works.",
+    "I'm just starting out as a Junior Software Engineer!",
+  ],
+  CV: "https://drive.google.com/uc?export=download&id=1fHjcp6SKFfzMBp2dVc4NgnK9wv776Kf6",
+};
+
+export default function Profile() {
+  return (
+    <div className="grid justify-items-center">
+      <div className="py-2">
+        <Image
+          src={profilePic}
+          width={180}
+          height={180}
+          alt="Picture of the author"
+          className="rounded-full aspect-square object-cover"
+        />
+      </div>
+      <div className="font-extrabold text-4xl py-2">{data.Name}</div>
+      <div className="flex py-2">
+        <div className="px-4 flex items-center space-x-2">
+          <BriefcaseIcon className="text-white" />
+          <span className="whitespace-nowrap">{data.Job}</span>
+        </div>
+        <div className="px-4 flex items-center space-x-2">
+          <LocationIcon className="text-white" />
+          <span className="whitespace-nowrap">{data.Location}</span>
+        </div>
+      </div>
+      <div className="italic py-2">{data.JobTitle}</div>
+      <Link
+        href={data.CV}
+        className="text-cyan-400 underline decoration-dotted decoration-2 hover:decoration-solid hover:text-cyan-500 py-2"
+      >
+        {"Meu currículo (PDF)"}
+      </Link>
+      <div className="text-center py-2">
+        {data.Description.map((item, index) => (
+          <div key={index} className="py-2">
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/assets/icons/BriefcaseIcon.tsx
@@ -0,0 +1,21 @@
+// src/components/icons/BriefcaseIcon.tsx
+import React from "react";
+
+interface BriefcaseIconProps {
+  className?: string; // Optional className for Tailwind styling
+}
+
+const BriefcaseIcon: React.FC<BriefcaseIconProps> = ({ className }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className={className} // Allow Tailwind classes to be passed
+  >
+    <path d="M22 9v11h-20v-11h20zm2-2h-24v15h24v-15zm-15-5c-1.104 0-2 .896-2 2v2h2v-1.5c0-.276.224-.5.5-.5h5c.276 0 .5.224.5.5v1.5h2v-2c0-1.104-.896-2-2-2h-6z" />
+  </svg>
+);
+
+export default BriefcaseIcon;
